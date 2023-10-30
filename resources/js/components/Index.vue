6ebbe0f06c077c988135278096e0bfcaa543758f
Dmitry <dmitrylancer.it@yandex.ru>
2023-10-30 20:19:32 +0300

    send images in backend

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,27 +1,44 @@
 <template>
     <div class="w-25">
-        <div ref="dropzone" class="btn d-block p-5 bg-dark text-center text-light">
+        <input v-model="title" type="text" class="mb-3 form-control" placeholder="title">
+        <div ref="dropzone" class="mb-3 btn d-block p-5 bg-dark text-center text-light">
             Upload
         </div>
+        <input @click.prevent="store" type="submit" class="btn btn-primary" value="add">
     </div>
 </template>
 
 <script>
 import Dropzone from 'dropzone'
+import axios from "axios";
 export default {
     name: "Index",
 
     data() {
         return {
             dropzone: null,
+            title: null,
         }
     },
 
     mounted() {
         this.dropzone = new Dropzone(this.$refs.dropzone, {
-            url: "/file/post"
+            url: "/api/posts",
+            autoProcessQueue: false,
         })
     },
+
+    methods: {
+        store() {
+            const images = new FormData()
+            const files = this.dropzone.getAcceptedFiles()
+            files.forEach(file => {
+                images.append('images[]', file)
+            })
+            axios.post('/api/posts', images)
+
+        }
+    }
 }
 </script>
 
